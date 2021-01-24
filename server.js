18f961994e46db0468f1afe7e653021c27c72777
Asli <aslinurkeklikci@yahoo.com>
2021-01-24 14:23:30 -0500

    updated server.js to fix bug

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
-
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-
-
-const PORT = process.env.PORT || 3000;
+const logger = require("morgan");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+
+// connect to mongo atlas db first then try local mongodb
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { 
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+});
 
-}, () => 
-console.log("Connected to DB")
-);
 
-// routes
+
+//                       PLACE ROUTES HERE!
+// ===================================================================
+
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
 
+app.get("/exercise", (res, req) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+
+// ======================================================================
+
+// Listen on port 3001
 app.listen(PORT, () => {
-  console.log(`🌎 App running on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
 });
